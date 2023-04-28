cc195eb79d1f0eaa9272c20e09f29d62e086c9f7
Daniel Lara Sanchez <daniellaraedinburgh@hotmail.com>
2023-04-28 22:52:19 +0100

    10

--- server.js
+++ server.js
@@ -1,52 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-io.on('connection', (socket) => {
-  console.log('User connected: ' + socket.id);
-  socket.on('join-room', (roomId) => {
+io.on("connection", (socket) => {
+  console.log("User connected: " + socket.id);
+  socket.on("join-room", (roomId) => {
     const roomClients = io.sockets.adapter.rooms.get(roomId) || new Set();
     const numberOfClients = roomClients.size;
 
     if (numberOfClients === 0) {
       console.log(`Creating room ${roomId} and emitting room_created`);
       socket.join(roomId);
-      socket.emit('room_created_server', roomId);
+      socket.emit("room_created_server", roomId);
       socket.remoteId = roomId;
     } else if (numberOfClients === 1) {
       console.log(`Joining room ${roomId} and emitting room_joined`);
       socket.join(roomId);
-      socket.emit('room_joined_server', roomId);
+      socket.emit("room_joined_server", roomId);
       socket.remoteId = roomId;
     } else {
       console.log(`Room ${roomId} is full, emitting room_full`);
-      socket.emit('room_full', roomId);
+      socket.emit("room_full", roomId);
     }
   });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected: ' + socket.id);
+  socket.on("disconnect", () => {
+    console.log("User disconnected: " + socket.id);
   });
 
-  socket.on('offer', (data) => {
+  socket.on("offer", (data) => {
+    console.log("Offer data received:", data);
     console.log(`Forwarding offer from ${socket.id} to ${data.target}`);
-    socket.to(data.target).emit('offer', { sdp: data.sdp, sender: socket.id });
+    socket.to(data.target).emit("offer", { sdp: data.sdp, sender: socket.id });
   });
 
-  socket.on('answer', (data) => {
+  socket.on("answer", (data) => {
+    console.log("Answer data received:", data);
     console.log(`Forwarding answer from ${socket.id} to ${data.target}`);
-    socket.to(data.target).emit('answer', { sdp: data.sdp, sender: socket.id });
+    socket.to(data.target).emit("answer", { sdp: data.sdp, sender: socket.id });
   });
 
-  socket.on('icecandidate', (data) => {
+  socket.on("icecandidate", (data) => {
+    console.log("ICE candidate data received:", data);
     console.log(`Forwarding ICE candidate from ${socket.id} to ${data.target}`);
-    socket.to(data.target).emit('icecandidate', { candidate: data.candidate, sender: socket.id });
+    socket
+      .to(data.target)
+      .emit("icecandidate", { candidate: data.candidate, sender: socket.id });
   });
 });
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
