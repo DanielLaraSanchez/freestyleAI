a274e32f32189901aa8cdc3ba060df1ecbd4d1a4
Daniel Lara Sanchez <daniellaraedinburgh@hotmail.com>
2023-04-30 15:34:11 +0100

    fix

--- public/app.js
+++ public/app.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const configuration = {
     iceServers: [
       {
-        urls: "stun:stun.l.google.com:19302",
+        urls: "stun:stun1.l.google.com:19302",
       },
     ],
   };
