4985abda962fcc449058222ec02789e37612d186
Daniel Lara Sanchez <daniellaraedinburgh@hotmail.com>
2023-05-21 21:50:21 +0100

    authentication works, now need to implement with socket.io

--- public/pages/auth/auth.js
+++ public/pages/auth/auth.js
@@ -7,67 +7,70 @@ document.addEventListener('DOMContentLoaded', function () {
     const signupBtn = document.getElementById('signup-btn');
 
     // Attach click event listeners
-    loginBtn.addEventListener('click', function () {
+    loginBtn.addEventListener('click', async function () {
+        console.log('signup button clicked'); // Add this line
+
         const nickname = document.getElementById('login-nickname').value;
         const password = document.getElementById('login-password').value;
-
+      
         if (nickname.trim() === '' || password.trim() === '') {
-            M.toast({ html: 'Please enter a nickname and password.' });
-            return;
+          M.toast({ html: 'Please enter a nickname and password.' });
+          console.log("here")
+          return;
         }
+        console.log("here")
 
-        // Send the request to the server
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                // Create a new URL object
-                const url = new URL('pages/battlefield/battlefield.html', window.location.origin);
+        try {
+            console.log('Sending login request...'); // Add this line
 
-                // Append the nickname parameter to the new URL
-                url.searchParams.append('nickname', encodeURIComponent(nickname));
+          const response = await fetch('/auth/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ nickname, password })
+          });
+          console.log('Received login response:', response); // Add this line
+          if (response.ok) {
+            console.log("Redirecting to battlefield..."); // Add this line
 
-                // Redirect to the new URL
-                window.location.href = url.href;
-            } else if (this.readyState === 4) {
-                M.toast({ html: 'Error: ' + this.responseText });
-            }
-        };
-        xhttp.open('POST', '/auth/login', true);
-        xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        xhttp.send(JSON.stringify({ nickname, password }));
-    });
+            location.href = '/battlefield';
+          } else {
+            const errorMessage = await response.text();
+            M.toast({ html: 'Error: ' + errorMessage });
+          }
+        } catch (error) {
+            console.log(error)
+          M.toast({ html: 'Error: ' + error.message });
+        }
+      });
 
-    signupBtn.addEventListener('click', function () {
+      signupBtn.addEventListener('click', async function () {
         const nickname = document.getElementById('signup-nickname').value;
         const password = document.getElementById('signup-password').value;
-
+      
         if (nickname.trim() === '' || password.trim() === '') {
-            M.toast({ html: 'Please enter a nickname and password.' });
-            return;
+          M.toast({ html: 'Please enter a nickname and password.' });
+          return;
         }
-
-        // Send the request to the server
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-              // Parse the response JSON and get the redirect URL
-              const response = JSON.parse(this.responseText);
-              const redirectUrl = response.redirectUrl;
-
-              // Create a new URL object
-              const url = new URL(redirectUrl, window.location.origin);
-
-              // Append the nickname parameter to the new URL
-              url.searchParams.append('nickname', encodeURIComponent(nickname));
-
-              // Redirect to the new URL
-              window.location.href = url.href;
-            } else if (this.readyState === 4) {
-              M.toast({ html: 'Error: ' + this.responseText });
-            }
-          };
-        xhttp.open('POST', '/auth/signup', true);
-        xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        xhttp.send(JSON.stringify({ nickname, password }));
-    });
+      
+        try {
+          const response = await fetch('/auth/signup', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ nickname, password })
+          });
+      
+          if (response.ok) {
+            location.href = '/battlefield';
+          } else {
+            const errorMessage = await response.text();
+            M.toast({ html: 'Error: ' + errorMessage });
+          }
+        } catch (error) {
+          M.toast({ html: 'Error: ' + error.message });
+        }
+      });
 });
--- server.js
+++ server.js
@@ -8,15 +8,20 @@ const cors = require("cors");
 const io = require("socket.io")(server);
 const bcrypt = require("bcryptjs");
 const { setupSocketEvents } = require("./signalingserver");
+const session = require("express-session");
 
-const uri = "mongodb+srv://f-raps-db:rXwglEkxGfL07wP8@cluster0.fnkdvcm.mongodb.net/?retryWrites=true&w=majority";
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const uri =
+  "mongodb+srv://f-raps-db:rXwglEkxGfL07wP8@cluster0.fnkdvcm.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function connectToMongoDB() {
@@ -41,9 +46,87 @@ const setMimeTypes = (res, filePath) => {
   }
 };
 
+// Configure Passport.js
+passport.use(
+  new LocalStrategy(
+    { usernameField: "nickname", passwordField: "password" },
+    async (nickname, password, done) => {
+          // Check for existing sessions
+    const db = client.db("f-raps-db");
+    const activeSessionsCollection = db.collection("ActiveSessions");
+
+    const existingSession = await activeSessionsCollection.findOne({ nickname });
+    if (existingSession) {
+      return done(null, false, { message: 'User already logged in' }); // Active session detected
+    }
+      // Add user authentication logic
+      const usersCollection = db.collection("User");
+
+      try {
+        const user = await usersCollection.findOne({ nickname });
+        if (user) {
+          // Check if password is correct using bcrypt
+          const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            user.active = true; // Add this line
+            done(null, user); // Success
+          } else {
+            done(null, false, { message: "Incorrect password" }); // Incorrect password
+          }
+        } else {
+          done(null, false, { message: "User not found" }); // User not found
+        }
+      } catch (error) {
+        done(error); // Error during authentication
+      }
+    }
+  )
+);
+passport.serializeUser((user, done) => {
+  done(null, user.nickname);
+});
+
+passport.deserializeUser(async (nickname, done) => {
+  const db = client.db("f-raps-db");
+  const usersCollection = db.collection("User");
+  
+  try {
+    const user = await usersCollection.findOne({ nickname });
+    
+    if (user) {
+      done(null, user);
+    } else {
+      done(new Error("User not found"), null);
+    }
+  } catch (error) {
+    done(error, null);
+  }
+});
+// Configure express-session middleware
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false, // should be false
+    cookie: { secure: process.env.NODE_ENV === 'production' }, // should only be secure in production environment
+  })
+);
+
+// Initialize Passport.js middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use((req, res, next) => {
   setMimeTypes(res, req.url);
-  express.static(path.join(__dirname, "public"), { setHeaders: setMimeTypes })(req, res, next);
+  express.static(path.join(__dirname, "public"), { setHeaders: setMimeTypes })(
+    req,
+    res,
+    next
+  );
 });
 
 app.get("/", (req, res) => {
@@ -63,67 +146,120 @@ app.get("/landing-page", (req, res) => {
 });
 
 app.get("/battlefield", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/pages/battlefield", "battlefield.html"));
-});
-
-app.get("/signout", (req, res) => {
-  // Perform any necessary sign-out actions, such as clearing session data
-
-  // Redirect the user to the desired page after sign-out
-  res.redirect("/auth");
-});
-
-app.post("/auth/login", async (req, res) => {
-  const { nickname, password } = req.body;
-
-  const db = client.db("f-raps-db");
-  const usersCollection = db.collection("User");
-
-  try {
-    const user = await usersCollection.findOne({ nickname });
-
-    if (!user) {
-      res.status(401).send("Invalid nickname or password");
-    } else {
-      // Compare the provided password with the stored hashed password
-      const isPasswordMatch = await bcrypt.compare(password, user.password);
-
-      if (isPasswordMatch) {
-        res.status(200).send({ message: "Login successful", redirectUrl: "pages/battlefield/battlefield.html" } );
-      } else {
-        res.status(401).send("Invalid nickname or password");
-      }
-    }
-  } catch (error) {
-    console.error("Error in /auth/login", error);
-    res.status(500).send("Server error");
+  console.log()
+  // Check if user is logged in
+  if (!req.session.loggedIn) {
+    res.redirect("/auth");
+  } else {
+    res.sendFile(
+      path.join(__dirname, "public/pages/battlefield", "battlefield.html")
+    );
   }
 });
 
-app.post("/auth/signup", async (req, res) => {
-  const { nickname, password } = req.body;
+app.get('/signout', async (req, res) => {
+  const db = client.db("f-raps-db");
+  const activeSessionsCollection = db.collection("ActiveSessions");
 
+  await activeSessionsCollection.deleteOne({ sessionId: req.session.id });
+  
+  // req.logout(); // Passport.js function to logout
+  req.session.loggedIn = true;
+  req.session.destroy(); // Destroy the session
+  res.redirect('/auth');
+});
+
+app.post("/auth/login", (req, res, next) => {
+  const db = client.db("f-raps-db");
+  console.log("works");
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.log("Error during authentication:", err);
+      return res.status(500).send("Server error");
+    }
+    if (!user) {
+      if (info && info.message) {
+        // Send the custom message from the LocalStrategy
+        return res.status(401).send(info.message);
+      } else {
+        console.log("No user found");
+        return res.status(401).send("Invalid nickname or password");
+      }
+    }
+
+    console.log("User found:", user);
+
+    req.logIn(user, async (err) => {
+      if (err) {
+        console.log("Error during logIn:", err);
+        return res.status(500).send("Server error");
+      }
+
+      console.log("Login successful");
+      req.session.loggedIn = true;
+      req.session.user = user;
+
+      // Insert session information into ActiveSessions collection
+      const activeSessionsCollection = db.collection("ActiveSessions");
+      await activeSessionsCollection.insertOne({
+        nickname: user.nickname,
+        sessionId: req.session.id,
+      });
+
+      return res.status(200).end();
+    });
+  })(req, res, next);
+});
+
+app.post("/auth/signup", async (req, res, next) => {
+  const { nickname, password } = req.body;
   const db = client.db("f-raps-db");
   const usersCollection = db.collection("User");
-
+  const activeSessionsCollection = db.collection("ActiveSessions");
   try {
     const existingUser = await usersCollection.findOne({ nickname });
+
+    // Check if the user is already logged in before signing up
+    const existingSession = await activeSessionsCollection.findOne({ nickname });
+    if (existingSession) {
+      return res.status(409).send("This user is already logged in");
+    }
 
     if (existingUser) {
       res.status(409).send("Nickname already in use");
     } else {
-      // Hash the password using bcrypt
       const hashedPassword = await bcrypt.hash(password, 10);
-
       await usersCollection.insertOne({ nickname, password: hashedPassword });
-      res.status(200).json({ message: "Signup successful", redirectUrl: "pages/battlefield/battlefield.html" });
+
+      passport.authenticate("local", (err, user) => {
+        if (err) {
+          return res.status(500).send("Server error");
+        }
+        if (!user) {
+          return res.status(401).send("Invalid nickname or password");
+        }
+        req.logIn(user, async (err) => {
+          if (err) {
+            return res.status(500).send("Server error");
+          }
+          req.session.loggedIn = true;
+          req.session.user = user;
+
+          // Insert session information into ActiveSessions collection
+          await activeSessionsCollection.insertOne({
+            nickname: user.nickname,
+            sessionId: req.session.id,
+          });
+
+          return res.status(200).end();
+        });
+      })(req, res, next);
     }
   } catch (error) {
     console.error("Error in /auth/signup", error);
     res.status(500).send("Server error");
   }
 });
-
 setupSocketEvents(io);
 
 const PORT = process.env.PORT || 3000;
