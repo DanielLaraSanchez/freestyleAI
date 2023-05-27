b09b17836f429d9c4bad423d8663d383d690df7a
Daniel Lara Sanchez <daniellaraedinburgh@hotmail.com>
2023-05-27 11:43:20 +0100

    fixing

--- public/pages/auth/auth.js
+++ public/pages/auth/auth.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
         try {
             console.log('Sending login request...'); // Add this line
 
-          const response = await fetch('/auth/login', {
+          const response = await fetch('https://dashboard.heroku.com/auth/login', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
           if (response.ok) {
             console.log("Redirecting to battlefield..."); // Add this line
 
-            location.href = '/battlefield';
+            location.href = 'https://dashboard.heroku.com/battlefield';
           } else {
             const errorMessage = await response.text();
             M.toast({ html: 'Error: ' + errorMessage });
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       
         try {
-          const response = await fetch('/auth/signup', {
+          const response = await fetch('https://dashboard.heroku.com/auth/signup', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
           });
       
           if (response.ok) {
-            location.href = '/battlefield';
+            location.href = 'https://dashboard.heroku.com/battlefield';
           } else {
             const errorMessage = await response.text();
             M.toast({ html: 'Error: ' + errorMessage });
