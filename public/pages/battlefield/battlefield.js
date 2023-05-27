4a9f692c650a5983adc3aea57299b4829076cf43
Daniel Lara Sanchez <daniellaraedinburgh@hotmail.com>
2023-05-27 14:12:03 +0100

    refctor battlefield.js

--- public/pages/battlefield/battlefield.js
+++ public/pages/battlefield/battlefield.js
@@ -1,42 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  function getCookieValue(name) {
-    const matchedCookie = document.cookie.match(new RegExp(name + "=([^;]+)"));
-    if (matchedCookie && matchedCookie[1]) {
-      const cookieValue = decodeURIComponent(matchedCookie[1]);
-      try {
-        const parsedValue = JSON.parse(cookieValue);
-        return parsedValue.nickname;
-      } catch (error) {
-        // If parsing fails, return the value as is
-        return cookieValue;
-      }
-    } else {
-      return null;
-    }
-  }
-  // Get the nickname from the URL
-  const myNickname = getCookieValue("fRapsUser");
-
-  // Add myNickname to the socket handshake query
-  const socket = io({ query: { nickname: myNickname } });
-
-  socket.on("clearCookies", () => {
-    document.cookie =
-      "fRapsUser=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-  });
-
-  let isInitiator = false;
+  //**********************************************
+  // REFERENCE DOM ELEMENTS AND SOCKET CONNECTION
+  //**********************************************
 
   const fightBtn = document.getElementById("fight-btn");
   const localVideoContainer = document.querySelector(".local-video-container");
-  const remoteVideoContainer = document.querySelector(
-    ".remote-video-container"
-  );
+  const remoteVideoContainer = document.querySelector(".remote-video-container");
   const modal = document.getElementById("modal");
   const closeModal = document.getElementById("close-modal");
+  const usersList = document.getElementById("users-list");
+  const countdownElement = document.getElementById("countdown");
+  const signoutBtn = document.getElementById("signout-btn");
+  const chatInput = document.getElementById("chat-input");
+  const chatSendBtn = document.getElementById("chat-send-btn");
+  const chatMessagesContainer = document.querySelector(".chat-messages-container");
 
   let opponentSocketId = null;
-  let peerConnection;
+  let isInitiator = false;
+
+  // WebRTC related
   const configuration = {
     iceServers: [
       {
@@ -60,6 +42,24 @@ document.addEventListener("DOMContentLoaded", () => {
     ],
   };
 
+  let peerConnection;
+
+  // Create video elements
+  const localVideo = document.createElement("video");
+  const remoteVideo = document.createElement("video");
+  localVideo.classList.add("local-video");
+  remoteVideo.classList.add("remote-video");
+  localVideoContainer.appendChild(localVideo);
+  remoteVideoContainer.appendChild(remoteVideo);
+
+  // Initialize socket connection
+  const socket = io({ query: { nickname: getCookieValue("fRapsUser") } });
+
+  //**********************************************
+  // EVENT LISTENERS
+  //**********************************************
+
+  // Fight button click event
   if (fightBtn) {
     fightBtn.addEventListener("click", async () => {
       modal.style.display = "block";
@@ -68,50 +68,147 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  // Close modal click event
   if (closeModal) {
     closeModal.addEventListener("click", () => {
       endRapBattle();
     });
   }
 
+  // Click outside modal event
   window.addEventListener("click", (event) => {
     if (event.target === modal) {
       endRapBattle();
     }
   });
 
+  // Sign out button click event
+  signoutBtn.addEventListener("click", async function () {
+    clearCookie("fRapsUser");
+    window.location.href = "/signout";
+  });
+
+  // Chat send button click event
+  chatSendBtn.addEventListener("click", () => {
+    if (chatInput.value !== "") {
+      socket.emit("chatMessage", chatInput.value);
+      chatInput.value = "";
+    }
+  });
+
+  // Chat input keyup event
+  chatInput.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13 && chatInput.value !== "") {
+      socket.emit("chatMessage", chatInput.value);
+      chatInput.value = "";
+    }
+  });
+
+  // Socket.io events
   socket.on("connect", () => {
     console.log("Connected to server");
   });
-
-  // WebRTC logic
-
-  const localVideo = document.createElement("video");
-  const remoteVideo = document.createElement("video");
-  localVideo.classList.add("local-video");
-  remoteVideo.classList.add("remote-video");
-  localVideoContainer.appendChild(localVideo);
-  remoteVideoContainer.appendChild(remoteVideo);
-  localVideo.muted = false;
-
-  initializePeerConnection();
 
   socket.on("foundOpponent", (data) => {
     opponentSocketId = data.socketId;
     isInitiator = data.isInitiator;
 
     initiateWebRTCConnection(isInitiator);
-
     if (isInitiator) {
-      startCountdown(10); // Start the countdown for 10 seconds
+      startCountdown(10);
     }
-
     socket.on("userDisconnected", (disconnectedSocketId) => {
       if (opponentSocketId === disconnectedSocketId) {
         endRapBattle();
       }
     });
   });
+
+  socket.on("receiveIceCandidate", async (data) => {
+    const candidate = new RTCIceCandidate(data.candidate);
+    await peerConnection.addIceCandidate(candidate);
+  });
+
+  socket.on("updateUserList", (users) => {
+    usersList.innerHTML = "";
+    users.forEach((user) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("user-item");
+      const avatarWrapper = document.createElement("div");
+      const avatar = document.createElement("i");
+      avatar.classList.add("material-icons");
+      avatar.textContent = "face";
+      avatarWrapper.appendChild(avatar);
+      listItem.appendChild(avatarWrapper);
+      const userNameWrapper = document.createElement("div");
+      const userName = document.createElement("span");
+      userName.textContent = user.socketId === socket.id ? "YOU" : user.nickname;
+      userNameWrapper.appendChild(userName);
+      listItem.appendChild(userNameWrapper);
+      usersList.appendChild(listItem);
+    });
+  });
+
+  socket.on("receiveOffer", async (data) => {
+    await peerConnection.setRemoteDescription(data.offer);
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    socket.emit("sendAnswer", {
+      answer: peerConnection.localDescription,
+      to: data.from,
+    });
+  });
+
+  socket.on("receiveAnswer", async (data) => {
+    await peerConnection.setRemoteDescription(data.answer);
+  });
+
+  socket.on("endRapBattle", () => {
+    endRapBattle();
+  });
+
+  socket.on("chatMessage", (data) => {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("chat-message");
+    const timeStamp = new Date(data.time).toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    messageElement.innerHTML = `
+          <div class="chat-message-info">
+            <span class="chat-message-sender">${data.nickname}</span>
+            <span class="chat-message-time">${timeStamp}</span>
+          </div>
+          <div class="chat-message-text">${data.message}</div>
+        `;
+    chatMessagesContainer.appendChild(messageElement);
+    chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
+  });
+
+  // Initialize and configure peer connection
+  initializePeerConnection();
+
+  //**********************************************
+  // UTILITY FUNCTIONS
+  //**********************************************
+
+  function getCookieValue(name) {
+    const matchedCookie = document.cookie.match(new RegExp(name + "=([^;]+)"));
+    if (matchedCookie && matchedCookie[1]) {
+      const cookieValue = decodeURIComponent(matchedCookie[1]);
+      try {
+        const parsedValue = JSON.parse(cookieValue);
+        return parsedValue.nickname;
+      } catch (error) {
+        // If parsing fails, return the value as is
+        return cookieValue;
+      }
+    } else {
+      return null;
+    }
+  }
+
   function startMediaCapture() {
     return new Promise(async (resolve, reject) => {
       try {
@@ -160,46 +257,8 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-  socket.on("receiveIceCandidate", async (data) => {
-    const candidate = new RTCIceCandidate(data.candidate);
-    await peerConnection.addIceCandidate(candidate);
-  });
-
-  socket.on("updateUserList", (users) => {
-    const usersList = document.getElementById("users-list");
-
-    // Clear the existing list
-    usersList.innerHTML = "";
-
-    // Add each user to the list
-    users.forEach((user) => {
-      const listItem = document.createElement("li");
-      listItem.classList.add("user-item");
-
-      const avatarWrapper = document.createElement("div");
-      const avatar = document.createElement("i");
-      avatar.classList.add("material-icons");
-      avatar.textContent = "face"; // Change the icon here
-      avatarWrapper.appendChild(avatar);
-      listItem.appendChild(avatarWrapper);
-
-      const userNameWrapper = document.createElement("div");
-      const userName = document.createElement("span");
-
-      // Use user.nickname instead of user.socketId
-      userName.textContent =
-        user.socketId === socket.id ? "YOU" : user.nickname;
-      userNameWrapper.appendChild(userName);
-      listItem.appendChild(userNameWrapper);
-
-      usersList.appendChild(listItem);
-    });
-  });
-
   function endRapBattle() {
-    // Remove event listener for "userDisconnected"
     socket.off("userDisconnected");
-
     if (localVideo.srcObject) {
       localVideo.srcObject.getTracks().forEach((track) => track.stop());
       localVideo.srcObject = null;
@@ -210,17 +269,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     opponentSocketId = null;
     modal.style.display = "none";
-
-    // Close the peer connection
     peerConnection.close();
-
-    // Initialize a new peer connection
     initializePeerConnection();
-
-    // Set isInitiator back to false
     isInitiator = false;
-
-    // Emit "leaveRoom" event to inform the server to reset the room
     socket.emit("leaveRoom");
   }
 
@@ -243,35 +294,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  socket.on("receiveOffer", async (data) => {
-    await peerConnection.setRemoteDescription(data.offer);
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
-    socket.emit("sendAnswer", {
-      answer: peerConnection.localDescription,
-      to: data.from,
-    });
-  });
-
-  socket.on("receiveAnswer", async (data) => {
-    await peerConnection.setRemoteDescription(data.answer);
-  });
-
-  socket.on("endRapBattle", () => {
-    if (localVideo.srcObject) {
-      localVideo.srcObject.getTracks().forEach((track) => track.stop());
-      localVideo.srcObject = null;
-    }
-    if (remoteVideo.srcObject) {
-      remoteVideo.srcObject.getTracks().forEach((track) => track.stop());
-      remoteVideo.srcObject = null;
-    }
-    opponentSocketId = null;
-    modal.style.display = "none";
-  });
-
   function startCountdown(duration) {
-    const countdownElement = document.getElementById("countdown");
     let remainingTime = duration;
 
     const updateCountdown = () => {
@@ -294,70 +317,4 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }, 1000);
   }
-
-  const signoutBtn = document.getElementById("signout-btn");
-
-  // Attach a click event listener
-  signoutBtn.addEventListener("click", async function () {
-    console.log("works")
-    // Clear the cookie
-    clearCookie("fRapsUser");
-
-    // Redirect the user to the sign-out route
-    window.location.href = "/signout";
-  });
-
-
-//************************************************************************************************************ */
-                  //CHAT
-//************************************************************************************************************* */                  
-  const chatInput = document.getElementById("chat-input");
-  const chatSendBtn = document.getElementById("chat-send-btn");
-  const chatMessagesContainer = document.querySelector(
-    ".chat-messages-container"
-  );
-
-  chatSendBtn.addEventListener("click", () => {
-      console.log("works chat button")
-    if (chatInput.value !== "") {
-      // Emit chat message event to the server
-      socket.emit("chatMessage", chatInput.value);
-      chatInput.value = "";
-    }
-  });
-
-  chatInput.addEventListener("keyup", (e) => {
-    if (e.keyCode === 13 && chatInput.value !== "") {
-      // Emit chat message event to the server
-      socket.emit("chatMessage", chatInput.value);
-      chatInput.value = "";
-    }
-  });
-
-  // Receive chat message from the server
-  socket.on("chatMessage", (data) => {
-    console.log(data, "message from browser")
-    const messageElement = document.createElement("div");
-    messageElement.classList.add("chat-message");
-
-    const timeStamp = new Date(data.time).toLocaleString("en-US", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    });
-
-    messageElement.innerHTML = `
-          <div class="chat-message-info">
-            <span class="chat-message-sender">${data.nickname}</span>
-            <span class="chat-message-time">${timeStamp}</span>
-          </div>
-          <div class="chat-message-text">${data.message}</div>
-        `;
-
-    chatMessagesContainer.appendChild(messageElement);
-
-    // Scroll to the latest message
-    chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
-  });
-  
 });
